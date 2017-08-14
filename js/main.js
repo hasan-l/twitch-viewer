e95b816ca8a8b901b25301e8d8558390299efeea
Hasan Liaquat <hasan.liaquat@hotmail.com>
2017-08-14 02:10:35 +0100

    rev module pattern implemented and minor improvs

--- js/main.js
+++ js/main.js
@@ -1,75 +1,101 @@
-$(document).ready(function(){
+var TwitchViewer = function(){
+	//cached jQuery objects
+	var $btnAll = $('.btn-all');
+	var $buttons = $('.btn');
+	var $notch = $('.notch');
+	var $stream = $(".stream");
+	var $statusIcon = $('.status i');
+	var $streamLinkArea = $('.stream a');
+	var $streamLogo = $(".stream a img");
 
-	//adding btn style
-	var btnAll = $('.btn-all');
-	btnAll.addClass('btn-bkgrnd-changed');
-	btnAll.find('.triangle').addClass('changed');
+	//adds initial btn style to btn lablled 'All'
+	$btnAll.addClass('btn-bkgrnd-changed');
+	$btnAll.find('.notch').addClass('changed');
 
-	$('.btn-all, .btn-online, .btn-offline').click(function(){
-		$('.btn-all, .btn-online, .btn-offline').removeClass('btn-bkgrnd-changed');
-		$('.triangle').removeClass('changed');
+	$buttons.click(function(){
+		//removes background colour on active btn
+		$buttons.removeClass('btn-bkgrnd-changed');
+		//removes notch on active btn
+		$notch.removeClass('changed');
 		
 		$(this).addClass('btn-bkgrnd-changed');
-		$(this).find('.triangle').addClass('changed');
+		$(this).find('.notch').addClass('changed');
 	});
 
 	var twitchStreamers = ["ESL_SC2", "OgamingSC2", "cretetion", "NALCS1", "storbeck", "habathcx", "RobotCaleb", "brunofin"];
 
-
 	for(var i = 0; i < twitchStreamers.length; i++){
 		
 		var channelName = document.querySelectorAll('.channel-name');
+		//add all the names of the streamers to the text content of the divs with class channel-name
 		channelName[i].textContent = twitchStreamers[i];
 
 		var twitchUrl = "https://wind-bow.gomix.me/twitch-api/streams/" + twitchStreamers[i] + "?callback=?";
 		(function(i){
+			//can't get streamers URL data from this call, so another is used below
 			$.getJSON(twitchUrl, function(data){
-				console.log(data);
-		
+
 				if(data.stream === null){
-					$('.status i').eq(i).addClass("fa-times-circle");
+					//channel offline so show an 'x' icon
+					//eq(i) finds element at the specificed index
+					$statusIcon.eq(i).addClass("fa-times-circle");
 				} else{
-					$('.status i').eq(i).addClass("fa-check");
+					//channel online
+					$statusIcon.eq(i).addClass("fa-check");
+					//get current status, i.e. what the channel is doing
 					var statusDesc = data.stream.channel.status;
 					if(statusDesc.length > 20){
 						statusDesc = statusDesc.substring(0, 20);
 					}
-					$('.stream a').eq(i).append("<div class='desc'><i>" + data.stream.channel.game + " - " + statusDesc + "...</i></div>");
+					//append channel status and current game that is being played as text right below channelName
+					$streamLinkArea.eq(i).append("<div class='desc'><i>" + data.stream.channel.game + " - " + statusDesc + "...</i></div>");
 				}
 			});
 
+			//couldn't get all info from single AJAX call (with this call, no data about if channel is online or not)
 			$.getJSON("https://wind-bow.gomix.me/twitch-api/channels/" + twitchStreamers[i] + "?callback=?", function(apiResult){
 				console.log(apiResult);
-				$(".stream a").eq(i).attr("href", apiResult.url);
+				//using the JSON data obtained from AJAX call, update href attribute of streamer
+				$streamLinkArea.eq(i).attr("href", apiResult.url);
+				//update src attribute for logo image
+				$streamLogo.eq(i).attr("src", apiResult.logo);
 
+				//if no logo or channel doesn't exist, show default cross image
+				if(apiResult.logo === null || apiResult.status === 404){
+					$streamLogo.eq(i).attr("src", "img/cross.jpg");
+				}
+
+				//specific case to show a streamer that doesn't exist
 				if(apiResult.status === 404){
 					channelName[i].textContent = "\"" + twitchStreamers[i]+ "\" doesn't exist";
-					$(".stream a img").eq(i).attr("src", "img/cross.jpg");
 				}
 			});
 
 		})(i);
 	}
 
-	btnAll.click(function(){
-		$(".stream").show();
+	//shows all streams
+	$btnAll.click(function(){
+		$stream.show();
 	});
 
 	$(".btn-online").click(function(){
-		$(".stream").show();
+		//reset by showing all and then remove streams that don't have a 'check'
+		$stream.show();
 		for(var j = 0; j < twitchStreamers.length; j++){
-			if(!$(".status i").eq(j).hasClass("fa-check")){
-				$(".stream").eq(j).hide();
+			if(!$statusIcon.eq(j).hasClass("fa-check")){
+				$stream.eq(j).hide();
 			}
 		}
 	});
 
 	$(".btn-offline").click(function(){
-		$(".stream").show();
+		//reset by showing all and then remove streams that don't have a 'cross'
+		$stream.show();
 		for(var j = 0; j < twitchStreamers.length; j++){
-			if(!$(".status i").eq(j).hasClass("fa-times-circle")){
-				$(".stream").eq(j).hide();
+			if(!$statusIcon.eq(j).hasClass("fa-times-circle")){
+				$stream.eq(j).hide();
 			}
 		}
 	});
-});
+}();
